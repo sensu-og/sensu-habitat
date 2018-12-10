c610bb21d94a68740ae40d130cb1ac2dd317943e
John Kerry <jk185160@ncr.com>
2018-12-10 08:02:05 -0500

    updating sensu-backend and agent to GA release 5.0.0

--- sensu-agent/plan.sh
+++ sensu-agent/plan.sh
@@ -1,12 +1,11 @@
 pkg_name=sensu-agent
-pkg_filename=sensu-agent
 pkg_origin=core
-pkg_version="2.0.0-beta.4-1"
+pkg_version="5.0.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
-pkg_source="https://storage.googleapis.com/sensu-binaries/$pkg_version/linux/amd64/$pkg_filename"
+pkg_source="https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/$pkg_version/sensu-go-$pkg_version-linux-amd64.tar.gz"
 pkg_svc_run="sensu-agent start -c $pkg_svc_config_path/agent.yml"
-pkg_shasum="0badb2a5776054419185ceede42f36adbc2a82a9639366f23f1a5f40819b6c12"
+pkg_shasum="1dac10ea8060e7d6f31c425ccb114ee678b8cbc08e51db95c17d90c873138b9b"
 pkg_deps=(core/curl)
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
@@ -15,16 +14,12 @@ pkg_binds_optional=(
 pkg_description="Sensu 2.0 Agent"
 pkg_upstream_url="https://sensu.io"
 
-do_unpack(){
-  return 0
-}
-
 do_build(){
   return 0
 }
 
 do_install() {
-  build_line "Installing $pkg_filename binary"
-  chmod +x "$HAB_CACHE_SRC_PATH/$pkg_filename"
-  install -D "$HAB_CACHE_SRC_PATH/$pkg_filename" "$pkg_prefix/bin/$pkg_filename"
+  build_line "Installing sensu-agent binary"
+  chmod +x "$HAB_CACHE_SRC_PATH/bin/sensu-agent"
+  install -D "$HAB_CACHE_SRC_PATH/bin/sensu-agent" "$pkg_prefix/bin/sensu-agent"
 }
--- sensu-backend/plan.sh
+++ sensu-backend/plan.sh
@@ -1,12 +1,11 @@
 pkg_name=sensu-backend
-pkg_filename=sensu-backend
 pkg_origin=sensu
-pkg_version="2.0.0-beta.4-1"
+pkg_version="5.0.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
-pkg_source="https://storage.googleapis.com/sensu-binaries/$pkg_version/linux/amd64/$pkg_filename"
+pkg_source="https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/$pkg_version/sensu-go-$pkg_version-linux-amd64.tar.gz"
 pkg_svc_run="sensu-backend start -c $pkg_svc_config_path/backend.yml"
-pkg_shasum="4fea31b3ffaaebc1c8ab4b487d5fae45f895a512d49123f494b18dc05c67533d"
+pkg_shasum="1dac10ea8060e7d6f31c425ccb114ee678b8cbc08e51db95c17d90c873138b9b"
 pkg_deps=(core/curl)
 pkg_bin_dirs=(bin)
 pkg_exports=(
@@ -16,16 +15,12 @@ pkg_exports=(
 pkg_description="Sensu 2.0 Backend"
 pkg_upstream_url="https://sensu.io"
 
-do_unpack(){
-  return 0
-}
-
 do_build(){
   return 0
 }
 
 do_install() {
-  build_line "Installing $pkg_filename binary"
-  chmod +x "$HAB_CACHE_SRC_PATH/$pkg_filename"
-  install -D "$HAB_CACHE_SRC_PATH/$pkg_filename" "$pkg_prefix/bin/$pkg_filename"
+  build_line "Installing sensu-backend binary"
+  chmod +x "$HAB_CACHE_SRC_PATH/bin/sensu-backend"
+  install -D "$HAB_CACHE_SRC_PATH/bin/sensu-backend" "$pkg_prefix/bin/sensu-backend"
 }

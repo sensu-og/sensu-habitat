911f31263cf11f1153ced8869f7110ef4c28e1d9
John Kerry <jk185160@ncr.com>
2018-12-14 03:49:57 -0500

    upgrading to version 5.0.1

--- sensu-agent/plan.sh
+++ sensu-agent/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=sensu-agent
 pkg_origin=sensu
-pkg_version="5.0.0"
+pkg_version="5.0.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
 pkg_source="https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/$pkg_version/sensu-go-$pkg_version-linux-amd64.tar.gz"
 pkg_svc_run="sensu-agent start -c $pkg_svc_config_path/agent.yml"
-pkg_shasum="1dac10ea8060e7d6f31c425ccb114ee678b8cbc08e51db95c17d90c873138b9b"
+pkg_shasum="26b5ad015e22858885a02160ef493b5418d50de65bfeacabb46f19bc3a736ce8"
 pkg_deps=(core/curl)
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
--- sensuctl/plan.sh
+++ sensuctl/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=sensuctl
 pkg_origin=sensu
-pkg_version="5.0.0"
+pkg_version="5.0.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
 pkg_source="https://s3-us-west-2.amazonaws.com/sensu.io/sensu-go/$pkg_version/sensu-go-$pkg_version-linux-amd64.tar.gz"
-pkg_shasum="1dac10ea8060e7d6f31c425ccb114ee678b8cbc08e51db95c17d90c873138b9b"
+pkg_shasum="26b5ad015e22858885a02160ef493b5418d50de65bfeacabb46f19bc3a736ce8"
 pkg_bin_dirs=(bin)
 pkg_description="Sensu 2.0 Backend"
 pkg_upstream_url="https://sensu.io"
